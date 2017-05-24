567bbe32b815ec1f5e36b3b9d90a445fff4d1462
fresh5447 <doug@kosmojo.com>
2017-05-24 16:11:28 -0600

    add form-and-delete

--- public/app.js
+++ public/app.js
@@ -2,7 +2,38 @@ const planeVue = new Vue({
   el: '#plane',
   data: {
     header: 'Plane Enthusiasts Club',
-    planes: []
+    planes: [],
+    newType: '',
+    newSpeed: '',
+    newImg: '',
+  },
+  methods: {
+    addNewPlane: function(event){
+      event.preventDefault();
+      let plane = {
+        type: this.newType,
+        topSpeed: this.newSpeed,
+        img: this.newImg
+      }
+      $.ajax({
+        url: '/api/planes',
+        method: 'POST',
+        data: plane
+      }).done((response) => {
+        console.log(response);
+        window.location = '/';
+      })
+    },
+    deletePlain: function(plane) {
+      let updatedPlanes = this.planes.filter((planeInArray) => {
+        return planeInArray._id !== plane._id
+      });
+      this.planes = updatedPlanes;
+      $.ajax({
+        url: `/api/planes/${plane._id}`,
+        method: 'DELETE'
+      }).done(response => console.log('Plane Deleted!'))
+    }
   }
 });
 
